4accb6cbad4ea17f003479eb7c3a5eff78ec83dd
Oscar Trujillo <oscartrugo@gmail.com>
2025-04-09 00:55:58 -0600

    8. Creando nuestro primer microservicio con Spring Boot y Cloud

---
+++ curso-kubernetes/msvc-usuarios/src/main/java/org/oscartrugo/springcloud/msvc/usuarios/msvc_usuarios/MsvcUsuariosApplication.java
@@ -0,0 +1,13 @@
+package org.oscartrugo.springcloud.msvc.usuarios.msvc_usuarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsvcUsuariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsvcUsuariosApplication.class, args);
+	}
+
+}
