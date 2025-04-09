d1676c6c086151fe45702138a4c9ebfd2bc1557d
Oscar Trujillo <oscartrugo@gmail.com>
2025-04-09 09:45:12 -0600

    13. Implementando el componente Service

---
+++ curso-kubernetes/msvc-usuarios/src/main/java/org/oscartrugo/springcloud/msvc/usuarios/msvc_usuarios/services/UsuarioService.java
@@ -0,0 +1,13 @@
+package org.oscartrugo.springcloud.msvc.usuarios.msvc_usuarios.services;
+
+import org.oscartrugo.springcloud.msvc.usuarios.msvc_usuarios.models.entity.Usuario;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface UsuarioService {
+    List<Usuario> listar();
+    Optional<Usuario> porId(Long id);
+    Usuario guardar(Usuario usuario);
+    void eliminar(Long id);
+}
