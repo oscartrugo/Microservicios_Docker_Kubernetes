2d42ff7712d09b0e0c0a2ecd7336406404de8e35
Oscar Trujillo <oscartrugo@gmail.com>
2025-04-11 12:31:30 -0600

    24. Escribiendo el controlador RestController para cursos

--- curso-kubernetes/msvc-cursos/src/main/java/org/oscartrugo/springcloud/msvc/cursos/CursoController.java
+++ curso-kubernetes/msvc-cursos/src/main/java/org/oscartrugo/springcloud/msvc/cursos/CursoController.java
@@ -3,10 +3,9 @@ package org.oscartrugo.springcloud.msvc.cursos;
 import org.oscartrugo.springcloud.msvc.cursos.entity.Curso;
 import org.oscartrugo.springcloud.msvc.cursos.services.CursoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
@@ -26,6 +25,33 @@ public class CursoController {
         Optional<Curso> o = service.porId(id);
         if(o.isPresent()) {
             return ResponseEntity.ok(o.get());
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    @PostMapping
+    public ResponseEntity<?> crear(@RequestBody Curso curso) {
+        Curso cursoDB = service.guardar(curso);
+        return ResponseEntity.status(HttpStatus.CREATED).body(cursoDB);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> editar(@RequestBody Curso curso, @PathVariable Long id) {
+        Optional<Curso> o = service.porId(id);
+        if(o.isPresent()) {
+            Curso cursoDb = o.get();
+            cursoDb.setNombre(curso.getNombre());
+            return ResponseEntity.status(HttpStatus.CREATED).body(service.guardar(cursoDb));
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> eliminar(@PathVariable Long id) {
+        Optional<Curso> o = service.porId(id);
+        if(o.isPresent()) {
+            service.eliminar(id);
+            return ResponseEntity.noContent().build();
         }
         return ResponseEntity.notFound().build();
     }
