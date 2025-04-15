3d7a0a68805e077b9868992deb8283d7ac6f88f4
Oscar Trujillo <oscartrugo@gmail.com>
2025-04-15 10:52:26 -0600

    34. Revisando tablas de la BBDD y agregando métodos de comunicación HTTP

--- curso-kubernetes/msvc-cursos/src/main/java/org/oscartrugo/springcloud/msvc/cursos/services/CursoService.java
+++ curso-kubernetes/msvc-cursos/src/main/java/org/oscartrugo/springcloud/msvc/cursos/services/CursoService.java
@@ -1,6 +1,7 @@
 package org.oscartrugo.springcloud.msvc.cursos.services;
 
 import org.oscartrugo.springcloud.msvc.cursos.models.entity.Curso;
+import org.oscartrugo.springcloud.msvc.cursos.models.Usuario;
 
 import java.util.List;
 import java.util.Optional;
@@ -10,4 +11,7 @@ public interface CursoService {
     Optional<Curso> porId(Long id);
     Curso guardar(Curso curso);
     void eliminar(Long id);
+    Optional<Usuario> asignarUsuario(Usuario usuario, Long cursoId);
+    Optional<Usuario> crearUsuario(Usuario usuario, Long cursoId);
+    Optional<Usuario> eliminarUsuario(Usuario usuario, Long cursoId);
 }
--- curso-kubernetes/msvc-cursos/src/main/java/org/oscartrugo/springcloud/msvc/cursos/services/CursoServiceImpl.java
+++ curso-kubernetes/msvc-cursos/src/main/java/org/oscartrugo/springcloud/msvc/cursos/services/CursoServiceImpl.java
@@ -1,5 +1,6 @@
 package org.oscartrugo.springcloud.msvc.cursos.services;
 
+import org.oscartrugo.springcloud.msvc.cursos.models.Usuario;
 import org.oscartrugo.springcloud.msvc.cursos.models.entity.Curso;
 import org.oscartrugo.springcloud.msvc.cursos.repositories.CursoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -37,5 +38,20 @@ public class CursoServiceImpl implements CursoService{
     @Transactional
     public void eliminar(Long id) {
         repository.deleteById(id);
+    }
+
+    @Override
+    public Optional<Usuario> asignarUsuario(Usuario usuario, Long id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<Usuario> crearUsuario(Usuario usuario, Long id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<Usuario> eliminarUsuario(Usuario usuario, Long cursoId) {
+        return Optional.empty();
     }
 }
