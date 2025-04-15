8fe10d40b34cb2e14da92689e9c2c69321e601aa
Oscar Trujillo <oscartrugo@gmail.com>
2025-04-14 23:52:48 -0600

    32. Creando JPA entity intermedia CursoUsuario

--- curso-kubernetes/msvc-cursos/src/main/java/org/oscartrugo/springcloud/msvc/cursos/entity/Curso.java
+++ curso-kubernetes/msvc-cursos/src/main/java/org/oscartrugo/springcloud/msvc/cursos/entity/Curso.java
@@ -3,6 +3,9 @@ package org.oscartrugo.springcloud.msvc.cursos.entity;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotEmpty;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 @Table(name = "cursos")
 public class Curso {
@@ -13,6 +16,13 @@ public class Curso {
 
     @NotEmpty
     private String nombre;
+
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<CursoUsuario> cursoUsuarios;
+
+    public Curso() {
+        cursoUsuarios = new ArrayList<>();
+    }
 
     public Long getId() {
         return id;
@@ -28,5 +38,21 @@ public class Curso {
 
     public void setNombre(String nombre) {
         this.nombre = nombre;
+    }
+
+    public void addCursoUsuario(CursoUsuario cursoUsuario) {
+        cursoUsuarios.add(cursoUsuario);
+    }
+
+    public void removeCursoUsuario(CursoUsuario cursoUsuario) {
+        cursoUsuarios.remove(cursoUsuario);
+    }
+
+    public List<CursoUsuario> getCursoUsuarios() {
+        return cursoUsuarios;
+    }
+
+    public void setCursoUsuarios(List<CursoUsuario> cursoUsuarios) {
+        this.cursoUsuarios = cursoUsuarios;
     }
 }
